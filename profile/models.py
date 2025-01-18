7423ac069e9da33e3f926fe6b2e93dad5915bc47
umidjonxikmatov <xikmatovumidjon1120@gmail.com>
2025-01-18 16:04:01 +0500

    delete

--- profile/models.py
+++ profile/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",  # Unique related_name
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",  # Unique related_name
+        blank=True,
+    )
